11441782f910202a2ea4d24ec09888735b00fead
Emmet175 <EmmetField@outlook.com>
2021-11-08 15:04:24 +0000

    Revert "Blah"
    This reverts commit 1d624c71bea24c781f7ec4207f2b12c0fc548286.

--- FYP/Assets/Scripts/Camera/CameraController.cs
+++ FYP/Assets/Scripts/Camera/CameraController.cs
@@ -46,12 +46,12 @@ public class CameraController : MonoBehaviour
         //elimination zoom
          if(eDist.playerDifference >= maxDistance)
         {
-            followDist -= 2 * Time.deltaTime;
+            followDist -= 3 * Time.deltaTime;
             verticalZoomScale = 0;
         }
         else if (eDist.playerDifference >= maxDistance + 3)
         {
-            followDist -= 4 * Time.deltaTime;
+            followDist -= 6 * Time.deltaTime;
             verticalZoomScale = 0;
         }
         else if(eDist.playerDifference <= 10)
