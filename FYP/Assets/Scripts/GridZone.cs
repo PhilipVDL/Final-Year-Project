91ec91c622e9a48f1fbbe1409417a71568129a43
PhilipVDL <61231557+PhilipVDL@users.noreply.github.com>
2021-11-29 20:59:27 +0000

    Grid basics

---
+++ FYP/Assets/Scripts/GridZone.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridZone : MonoBehaviour
+{
+    public int x, z;
+    public bool filled;
+    public GameObject obstacle;
+}
