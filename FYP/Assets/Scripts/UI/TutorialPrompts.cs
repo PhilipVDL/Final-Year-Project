3bb5c13a6054582e96941b2d3e8740639a0a7ff2
RyanByrne <c18430266@mydit.ie>
2022-04-13 17:46:20 +0100

    Prompt box art needed

--- FYP/Assets/Scripts/UI/TutorialPrompts.cs
+++ FYP/Assets/Scripts/UI/TutorialPrompts.cs
@@ -11,29 +11,34 @@ public class TutorialPrompts : MonoBehaviour
 
     private bool moveTut = false;
     private bool jumpTut = false;
+    private bool pickUpTut = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
-    void MovePrompt()
-    {
-        if(Player.GetComponent<PlayerController>().speeding == true && moveTut == false)
-        {
-            
-            promptBoxes[0].SetActive(true);
-            moveTut = true;
-        }
-    }
 
-    void JumpPrompt()
+    void Prompts()
     {
         if(Player.GetComponent<PlayerController>().grounded != true && jumpTut == false)
         {
             promptBoxes[1].SetActive(true);
             jumpTut = true;
         }
+
+        if (Player.GetComponent<PlayerController>().speeding == true && moveTut == false)
+        {
+            promptBoxes[0].SetActive(true);
+            moveTut = true;
+        }
+
+        if(Player.GetComponent<ObstacleInventory>().obstacles[0] != null && pickUpTut == false)
+        {
+            promptBoxes[2].SetActive(true);
+            pickUpTut = true;
+        }
+
     }
 
     void EndPrompts()
@@ -43,21 +48,23 @@ public class TutorialPrompts : MonoBehaviour
             duration -= Time.deltaTime;
         }
 
-        
+        if (duration <= 0)
+        {
+            promptBoxes[0].SetActive(false);
+            promptBoxes[1].SetActive(false);
+            promptBoxes[2].SetActive(false);
+            duration = 0;
+        }
+
     }
     
      void Update()
     {
         Player = this.gameObject;
-        MovePrompt();
-        JumpPrompt();
+        Prompts();
+        
         EndPrompts();
 
-        if (duration <= 0)
-        {
-            promptBoxes[0].SetActive(false);
-            promptBoxes[1].SetActive(false);
-            duration = 0;
-        }
+       
     }
 }
