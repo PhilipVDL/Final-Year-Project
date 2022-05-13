30c471504b43cfb28c5a5e9f3f082b155d7c019e
PhilipVDL <61231557+PhilipVDL@users.noreply.github.com>
2022-05-13 14:09:47 +0100

    Prompt bug fix

--- FYP/Assets/Scripts/UI/TutorialPrompts.cs
+++ FYP/Assets/Scripts/UI/TutorialPrompts.cs
@@ -10,9 +10,11 @@ public class TutorialPrompts : MonoBehaviour
     ObstacleInventory obsInv;
 
     public float duration;
+    private float _duration = 3;
 
     private bool moveTut = false;
     private bool jumpTut = false;
+    private bool hasJumped = false;
     private bool pickUpTut = false;
 
     void Start()
@@ -24,7 +26,34 @@ public class TutorialPrompts : MonoBehaviour
 
     void Prompts()
     {
-        if(controller.grounded != true && jumpTut == false)
+        if (!moveTut)
+        {
+            promptBoxes[0].SetActive(true); //active moveTut at start
+            moveTut = true;
+        }
+        else if(moveTut && !jumpTut && controller.speeding)
+        {
+            promptBoxes[1].SetActive(true); //activate jumpTut if player has moved after moveTut
+            jumpTut = true;
+        }
+        else if(moveTut && jumpTut && !controller.grounded)
+        {
+            hasJumped = true;
+            duration = _duration;
+        }
+
+        if (obsInv.obstacles.Count > 0)
+        {
+            if (obsInv.obstacles[0] != null && pickUpTut == false)
+            {
+                promptBoxes[2].SetActive(true); //activate pickupTut if player got a pickup
+                pickUpTut = true;
+                duration = _duration;
+            }
+        }
+
+        /*
+        if (controller.grounded != true && jumpTut == false)
         {
             promptBoxes[1].SetActive(true);
             jumpTut = true;
@@ -35,31 +64,40 @@ public class TutorialPrompts : MonoBehaviour
             promptBoxes[0].SetActive(true);
             moveTut = true;
         }
-
-        if(obsInv.obstacles.Count > 0)
-        {
-            if (obsInv.obstacles[0] != null && pickUpTut == false)
-            {
-                promptBoxes[2].SetActive(true);
-                pickUpTut = true;
-            }
-        }
+        */
     }
 
     void EndPrompts()
     {
-        if(moveTut == true && jumpTut == true && duration > 0)
+        if(pickUpTut && duration > 0)
         {
-            duration -= Time.deltaTime;
+            duration -= Time.deltaTime; //timer for disabling pickup Tut
+        }
+        else if(moveTut && jumpTut && hasJumped && duration > 0)
+        {
+            duration -= Time.deltaTime; //timer for disabling move and jump Tuts
+        }
+        else if(pickUpTut && duration <= 0)
+        {
+            promptBoxes[2].SetActive(false); //disable pickup Tut
+            duration = 0;
+        }
+        else if (moveTut && jumpTut && duration <= 0)
+        {
+            promptBoxes[0].SetActive(false); //disable move and jump Tuts
+            promptBoxes[1].SetActive(false);
+            duration = 0;
         }
 
+        /*
         if (duration <= 0)
         {
             promptBoxes[0].SetActive(false);
             promptBoxes[1].SetActive(false);
-            promptBoxes[2].SetActive(false);
+            //promptBoxes[2].SetActive(false);
             duration = 0;
         }
+        */
     }
 
     void Update()
